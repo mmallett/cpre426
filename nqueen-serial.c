ae461a6647da1d737077343f26c05f752f124c30
mmallett <mmallett91@gmail.com>
2013-03-11 00:39:49 -0500

    single answer done

--- nqueen-serial.c
+++ nqueen-serial.c
@@ -10,21 +10,22 @@ void solve(int col, int *hist)
 {
 	if(done) return; //break out of recursion, answer found
 
+	int i, j;
+	
 	if (col == N) {
-		for(i=0; i< N; i++, printf("%d", hist[i]+1));
+		for(i=0; i< N; i++, printf("%d ", hist[i]+1));
 		printf("\n");
 		done = 1;
 		return;
 	}
  
 #	define attack(i, j) (hist[j] == i || abs(hist[j] - i) == col - j)
-	int j;
 	for (i = 0, j = 0; i < N; i++) {
 		for (j = 0; j < col && !attack(i, j); j++);
 		if (j < col) continue;
  
 		hist[col] = i;
-		solve(N, col + 1, hist);
+		solve(col + 1, hist);
 	}
 }
  
@@ -44,13 +45,13 @@ int main(int argc, char **argv)
 	if(rank == 0)
 	{
 		int * hist = (int*) malloc(N * sizeof(int));
-		solve(N, 0, hist);
+		solve(0, hist);
 	}
 	
 	MPI_Barrier(MPI_COMM_WORLD);
 	end = MPI_Wtime();
 	
-	printf("RUNTIME %lf ms\n", (end - start) * 1000);
+	if(rank == 0) printf("RUNTIME %lf ms\n", (end - start) * 1000);
 	
 	MPI_Finalize();
 	
