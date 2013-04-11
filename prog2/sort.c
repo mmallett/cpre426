aa130dd87f08f25323cf924b6a2fe04556c82fb9
mmallett <mmallett91@gmail.com>
2013-04-10 19:07:49 -0500

    i hate debugging =[

--- prog2/sort.c
+++ prog2/sort.c
@@ -1,282 +1,392 @@
 /*
-Matt Mallett	mmallett@iastate.edu
-Deven Starn		djstarn@gmail.com
+Matt Mallett mmallett@iastate.edu
+Deven Starn djstarn@gmail.com
 
 CprE 426 Programming Assignment 2
 sort.c
-parallelized quicksort
+parallelized quicksort via MPI
 */
 
 #include <stdio.h>
-#include <sdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <mpi.h>
 
-#define BIG 0x3FFFFFFF
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>>>>CHANGE THESE<<<<<<<<<<<<<<<<<<<<<<<<<<<
+*/
+static const char IN_PATH[] = "/home/mmallett/cpre426/prog2/in.txt"; //path to input file
+static const char OUT_PATH[] = "/home/mmallett/cpre426/prog2/out/"; //path to directory where output will go
+//note this ends in a /
+/*
+>>>>>>>>>>>>>>>>>>>>>>>>>>>CHANGE THESE<<<<<<<<<<<<<<<<<<<<<<<<<<<
+*/
 
-int main(int argc, char ** argv)
-{	
+int main(int argc, char ** argv){
 	MPI_Init(&argc, &argv);
-	int world_rank;
+	int world_rank, world_size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	//variables holding state of processor's share of data
-	int * data;
+	int * rec_data;
+	int   rec_cnt;
+
 	int * send_data;
-	int data_size;
-	
-	int m; //problem size
+	int * send_cnt;
+	int * send_disp;
 
-	if(world_rank == 0)
-	{	
-		//open the file
+	int n;
+
+	if(world_rank == 0){
 		FILE * fp;
-		fp = fopen("/home/mmallett/cpre426/prog2/in.txt", "r");
+		fp = fopen(IN_PATH, "r");
 
-		if(fp == NULL)
-		{
+		if(fp == NULL){
+			perror("Could not open file");
+			exit(EXIT_FAILURE);
+		}
+
+		//first line is problem size
+		fscanf(fp, "%d\n", &n);
+
+		int i;
+		send_data = (int*) calloc(n, sizeof(int));
+
+		//fill buffer with data from file
+		for(i=0; i<n; i++){
+			fscanf(fp, "%d\n", &send_data[i]);
+		}
+
+		fclose(fp);
+
+		send_cnt = (int*) calloc(world_size, sizeof(int));
+		send_disp = (int*) calloc(world_size, sizeof(int));
+
+		//calculate vector sizes for each processor
+		for(i=0; i<world_size; i++){
+			send_cnt[i] = n/world_size;
+		}
+		send_cnt[0] += n%world_size;
+
+		//calculate vector displacements for each processor
+		send_disp[0] = 0;
+		for(i=1; i<world_size; i++){
+			send_disp[i] = send_disp[i-1]+send_cnt[i-1];
+		}
+	}
+
+	//scatter incoming vector sizes to processors
+	MPI_Scatter(
+		send_cnt,
+		1,
+		MPI_INT,
+		&rec_cnt,
+		1,
+		MPI_INT,
+		0,
+		MPI_COMM_WORLD
+	);
+
+	rec_data = (int*) calloc(rec_cnt, sizeof(int));
+
+	//scatter data to processors
+	MPI_Scatterv(
+		send_data,
+		send_cnt,
+		send_disp,
+		MPI_INT,
+		rec_data,
+		rec_cnt,
+		MPI_INT,
+		0,
+		MPI_COMM_WORLD
+	);
+
+	////////////////////////////////////////////////////////
+	// END DISTRIBUTION STAGE
+	// SORTING STAGE BEGIN
+	////////////////////////////////////////////////////////
+
+	int * data = rec_data;
+	int data_length = rec_cnt;
+
+	int m;
+	int q = world_size;
+
+	MPI_Bcast(
+		&n,
+		1,
+		MPI_INT,
+		0,
+		MPI_COMM_WORLD
+	);
+
+	m = n;
+	
+	MPI_Comm comm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &comm);
+
+	srand(18);
+
+	while(q>1){
+		int rank;
+		MPI_Comm_rank(comm, &rank);
+		
+		//figure out who has the kth integer
+		int k = rand()%(m-1);
+		
+		int * sizes = (int*) calloc(q, sizeof(int));
+		MPI_Allgather(
+			&data_length,
+			1,
+			MPI_INT,
+			sizes,
+			q,
+			MPI_INT,
+			comm
+		);
+
+		int i;
+		int data_length_total = sizes[0];
+		int has_k = 0;
+		for(i=1; i<q; i++){
+			data_length_total += sizes[i];
+			sizes[i] += sizes[i-1];
+			if(k >= sizes[i-1] && k < sizes[i]){
+				has_k = i;
+			}
+		}
+
+		int k_val;
+		if(has_k == rank){
+			if(rank == 0){
+				k_val = data[k];
+			}
+			else{
+				k_val = data[k-sizes[rank-1]];
+			}
+		}
+		
+		//broadcast k value to everyone
+		MPI_Bcast(
+			&k_val,
+			1,
+			MPI_INT,
+			has_k,
+			comm
+		);
+			
+		//partition data around k
+		int left = 0, right = data_length-1;
+		while(left < right){
+			while(data[left] <= k_val) left++;
+			while(data[right] > k_val) right--;
+			if(left < right){//swap
+				int tmp = data[left];
+				data[left] = data[right];
+				data[right] = tmp;
+			}
+		}
+
+		int left_size = right;
+		int right_size = data_length - right;
+
+		int * left_sizes = (int*) calloc(q, sizeof(int));
+	
+		MPI_Allgather(
+			&left_size,
+			1,
+			MPI_INT,
+			left_sizes,
+			q,
+			MPI_INT,
+			comm
+		);
+
+		int left_total = 0;
+		for(i=0; i<q; i++){
+			left_total += left_sizes[i];
+		}
+		int right_total = data_length - left_total;
+
+		//allocate processors to the 2 partitions
+		int available = q - 2;
+		int left_group_size = (left_total/data_length)*available + 1;
+		int right_group_size = q - left_group_size;
+
+		//split up buffer for distribution
+		free(send_cnt);
+		free(send_disp);
+		send_cnt = (int*) calloc(q, sizeof(int));
+		send_disp = (int*) calloc(q, sizeof(int));
+
+		int left_buff_size = left_size/left_group_size;
+		for(i=0; i<left_group_size; i++){
+			send_cnt[i] = left_buff_size;
+		}
+		send_cnt[left_group_size-1] += left_size%left_group_size;
+
+		int right_buff_size = right_size/right_group_size;
+		for(i=left_group_size; i<q; i++){
+			send_cnt[i] = right_buff_size;
+		}
+		send_cnt[q-1] += right_size/right_group_size;
+
+		send_disp[0] = 0;
+		for(i=1; i<q; i++){
+			send_disp[i] = send_disp[i-1] + send_cnt[i-1];
+		}
+
+		//send the vector sizes
+		int * recv_cnt = (int*) calloc(q, sizeof(int));
+		MPI_Alltoall(
+			send_cnt,
+			1,
+			MPI_INT,
+			recv_cnt,
+			1,
+			MPI_INT,
+			comm
+		);
+		
+		//prepare arguments
+		int total_inc_size = 0;
+		int * recv_disp = (int*) calloc(q, sizeof(int));
+		for(i=0; i<q; i++){
+			total_inc_size += recv_cnt[i];
+		}
+		recv_disp[0] = 0;
+		for(i=1; i<q; i++){
+			recv_disp[i] = recv_disp[i-1] + recv_cnt[i-1];
+		}
+
+		rec_data = (int*) calloc(total_inc_size, sizeof(int));
+
+		//send the data!
+		MPI_Alltoallv(
+			data,
+			send_cnt,
+			send_disp,
+			MPI_INT,
+			rec_data,
+			recv_cnt,
+			recv_disp,
+			MPI_INT,
+			comm
+		);
+
+		//housekeeping
+		free(data);
+		data = rec_data;
+		data_length = total_inc_size;
+
+		free(recv_cnt);
+		free(recv_disp);
+		
+		//split communicator in 2
+		int color = rank < left_group_size;
+		int new_rank = (color) ? rank : rank - left_group_size;
+
+		MPI_Comm new_comm;
+		MPI_Comm_split(
+			comm,
+			color,
+			new_rank,
+			&new_comm
+		);
+		comm = new_comm;
+
+		MPI_Comm_size(comm, &q);
+	}//end while
+
+	///////////////////////////////////////////////////////
+	// PARALLEL SORTING DONE
+	// BEGIN SERIAL SORT
+	///////////////////////////////////////////////////////
+
+	//COMMENCE LOLBADSORT
+	int i,j,min;
+	for(j=0; j<data_length-1; j++){
+		min=j;
+		for(i=j+1; i<data_length; i++){
+			if(data[i] < data[min]){
+				min=i;
+			}
+		}
+	
+		if(min != j){
+			int tmp = data[j];
+			data[j] = data[min];
+			data[min] = tmp;
+		}
+	}
+	
+	///////////////////////////////////////////////////////
+	// END SORTING
+	// BEGIN OUTPUT SECTION
+	///////////////////////////////////////////////////////
+	
+	char path_buffer[50];
+	strcpy(path_buffer, OUT_PATH);
+	sprintf(path_buffer, "%spart%d.txt", path_buffer, world_rank);
+	
+	FILE * out;
+	out=fopen(path_buffer, "w");
+	
+	if(out == NULL){
+			perror("Could not open file");
+			exit(EXIT_FAILURE);
+	}
+	
+	for(i=0; i<data_length-1; i++){
+		fprintf(out, "%d\n", data[i]);
+	}
+	
+	fclose(out);
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+	if(world_rank == 0){
+		strcpy(path_buffer, OUT_PATH);
+		sprintf(path_buffer, "%ssorted.txt", path_buffer);
+		
+		out = fopen(path_buffer, "w");
+		
+		if(out == NULL){
 			perror("Could not open file");
 			exit(EXIT_FAILURE);
 		}
 		
-		int m;
-		fscanf(fp, "%d\n", &m);
-		//printf("%d elements\n", n);	
-
-		int i;
-		data_size = m/world_size + (m%world_size > 0);
-		int buff_size = world_size * data_size;
-		send_data = (int*) malloc(buff_size *sizeof(int));
-
-		//fill buffer with data from file		
-		for(i=0; i<m; i++)
-		{
-			fscanf(fp, "%d\n", &send_data[i]);
-		}
-		//fill remainder of buffer, gives each processor same buffer size
-		for(i=m; i<buff_size; i++)
-		{
-			send_data[i] = BIG;
-		}
+		for(i=0; i<world_size; i++){
+			strcpy(path_buffer, OUT_PATH);
+			sprintf(path_buffer, "%spart%d.txt", path_buffer, i);
 			
+			FILE * readin;
+			readin = fopen(path_buffer, "r");
+			
+			if(readin == NULL){
+				fprintf(out, "part %d could not be read\n", i);
+				continue;
+			}
+			
+			//read in write to sorted.txt
+			int c;
+			while((c = fgetc(readin)) != EOF){
+				fputc(c, out);
+			}
+			
+			fclose(readin);
+		}
+		
+		fclose(out);
 	}
 	
-	//broadcast problem size .. might not be needed we will see
-	MPI_Bcast
-	(
-		&m,
-		1,
-		MPI_INT,
-		0,
-		MPI_COMM_WORLD
-	);
-
-	//broadcast size of incound data buffer
-	MPI_Bcast
-	(
-		&data_size,
-		1,
-		MPI_INT,
-		0,
-		MPI_COMM_WORLD
-	);
-
-	data = (int*) malloc(data_size * sizeof(int));
-
-	//scatter data among processors
-	MPI_Scatter
-	(
-		send_data,
-		data_size,
-		MPI_INT,
-		data,
-		data_size,
-		MPI_INT,
-		0,
-		MPI_COMM_WORLD
-	);
-	
-	///////////////////////////////////////////////////////////////
-	// DATA DISTRIBUTED
-	// PARTITION NOW
-	///////////////////////////////////////////////////////////////
-	
-/*int main(int argc, char** argv)
-{
-        MPI_Init(&argc, &argv);
-        MPI_Comm comm;
-        int tosort[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};*/
-        //int m = sizeof(tosort) / sizeof(tosort[0]);
-        //Get the size of the comm
-        int q = world_size;
-        //MPI_Comm_size(comm, &q);
-        int color_id = 0;
-
-		while(q > 1){
-        /*if(q == 1){
-                //Sort serially.
-                //Print to file.
-        }*/
-        //else {
-                //1. If q>1 all processors in the communicator use the same random number
-                //   generator to generate a random number between 0 and m-1, say k.
-
-                srand(123);
-                int k = rand() % m;
-
-                if(k == 0)
-                {
-                        k = 1;
-                }
-
-                //See if this processor has the kth integer
-                int rank;
-                MPI_Comm_rank(comm, &rank);
-
-                int offset = rank * m;
-                int offset_top = offset + ((rank + 1) * m);
-                int pivot = 0;
-
-                //2. The processor that has the kth integer (pivot) broadcasts it to all
-                //   processors in the communicator.
-
-                //If kth integer broadcast its value to all processors in group
-                if(k > offset && k < offset_top)
-                {
-                        pivot = tosort[k];
-                        //kth integer is in this process, broadcast it to all other processors
-                        MPI_Bcast(&pivot, 1, MPI_INT, 0, comm);
-                }
-
-                MPI_Barrier(comm);
-
-				//3. Each processor goes through its integer array and partitions it into two sub-arrays
-                //   one containing integers less than or equal to the pivot and another containing
-                //   integers  greater than the pivot.
-
-                //Find the number of elements greater than pivot
-                int num_greater_than_pivot = 0;
-                int num_less_than_pivot = 0;
-                int i;
-                for(i = offset; i < offset_top; i++)
-                {
-                        if(tosort[i] > pivot)
-                        {
-                                num_greater_than_pivot++;
-                        }
-                        else {
-                                num_less_than_pivot++;
-                        }
-                }
-                //Create the arrays to contained partitioned integers
-                int m_double_prime[num_greater_than_pivot];
-                int m_prime[num_less_than_pivot];
-
-
-                //Partition the array.
-                int j = 0;
-                for(i = offset; i < offset_top; i++)
-                {
-                        if(tosort[i] > pivot)
-                        {
-                                m_double_prime[j] = tosort[i];
-                        }
-                        else {
-                                m_prime[j] = tosort[i];
-                        }
-
-                        j++;
-                }
-
-                //4. Using an allgather operation, the number of integers in each of the two
-                //   sub-arrays on all the processors is gathered on every processor.
-
-                //Allgather the number of elements in each subarray
-                int *m_prime_buf;
-                int *m_double_prime_buf;
-
-                m_prime_buf = (int *)malloc(q*sizeof(int));
-                m_double_prime_buf = (int *)malloc(q*sizeof(int));
-
-                MPI_Allgather(&num_greater_than_pivot, 1, MPI_INT, &m_double_prime_buf, 1, MPI_INT, comm);
-                MPI_Allgather(&num_less_than_pivot, 1, MPI_INT, &m_prime_buf, 1, MPI_INT, comm);
-
-                MPI_Barrier(comm);
-
-                //5. Compute the total number of integers less than or equal to the pivot and the total
-                //   number of integers greater than the pivot.
-
-                int m_prime_total = 0;
-                int m_double_prime_total = 0;
-
-                for(i = q; i < q; i++)
-                {
-                        m_prime_total = m_prime_total + m_prime_buf[i];
-                        m_double_prime_total = m_double_prime_total + m_double_prime_buf[i];
-                }
-
-				//6. Parition the p processors to the two sub-problems of sorting m' integers and sorting m''
-                //   integers respectively, by allocating processors in proportion to the problem sizes(make
-                //   sure you do not allocate zero processors to a problem).
-
-                //Each subarray must have a processor
-                int size_to_allocate = q - 2;
-
-                //Find the number of processors each subarray needs
-                int less_than_allocation;
-                int greater_than_allocation;
-
-                if(m_prime_total > m_double_prime_total)
-                {
-                        less_than_allocation =
-                                ((m_double_prime_total * size_to_allocate) + m_prime_total - 1) / m_prime_total;
-                        greater_than_allocation = size_to_allocate - less_than_allocation;
-                }
-                else {
-                        greater_than_allocation =
-                                ((m_prime_total * size_to_allocate) + m_double_prime_total - 1) / m_double_prime_total;
-                }
-
-                //Find greater of two to sift by
-                int sift;
-
-                if(less_than_allocation > greater_than_allocation)
-                {
-                        sift = less_than_allocation;
-                }
-                else {
-                        sift = greater_than_allocation;
-                }
-
-                if(rank == 0)
-                {
-                        //Set to lower sift
-                        MPI_Comm_split(comm, 1, 0, &new_comm);
-                }
-                else if(rank == (q-1))
-                {
-                        //Set to higher sift
-                        MPI_Comm_split(comm, 2, 0, &new_comm);
-                }
-                else if(rank < sift)
-                {
-                        //Set to lower sift
-                        MPI_Comm_split(comm, 1, rank, &new_comm);
-                }
-                else
-                {
-                        //Set to higher sift
-                        MPI_Comm_split(comm, 2, (rank - sift), &new_comm);
-                }
-
-                //7. Using the gathered information on the sizes of the sub-arrays on all processors
-                //   each processor can compute where to send its data and from where to recieve.
-
-                //8. Use an Alltoall communication to perform the data transfer. Create two new
-                //   communicators corresponding to the new paritions.
-
-                //9. Recursively sort within each partition.
-                //Set old comm to new comm
-        }
+	MPI_Finalize();
+	return 0;
 }
+			
+	 
+
+
+	
